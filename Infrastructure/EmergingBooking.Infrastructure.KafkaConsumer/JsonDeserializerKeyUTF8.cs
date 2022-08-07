82b1ce73093a784fb600481fc5fdf5b9d8aea943
Alexandre Taumaturgo <magno.alexandre@greenbeltti.onmicrosoft.com>
2022-08-07 12:44:33 -0300

    adding kafka consumer

---
+++ Infrastructure/EmergingBooking.Infrastructure.KafkaConsumer/JsonDeserializerKeyUTF8.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using Confluent.Kafka;
+using Newtonsoft.Json;
+
+namespace EmergingBooking.Infrastructure.KafkaConsumer;
+
+internal class JsonDeserializerKeyUTF8<TKeyType> : IDeserializer<TKeyType>
+{
+    private readonly Encoding _encoder;
+
+    public JsonDeserializerKeyUTF8()
+    {
+        _encoder = Encoding.UTF8;
+    }
+
+    public TKeyType Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
+    {
+        return JsonConvert.DeserializeObject<TKeyType>(_encoder.GetString(data.ToArray()));
+    }
+}
---
+++ Infrastructure/EmergingBooking.Infrastructure.KafkaConsumer/JsonDeserializerValueUTF8.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using Confluent.Kafka;
+using Newtonsoft.Json;
+
+namespace EmergingBooking.Infrastructure.KafkaConsumer;
+
+internal class JsonDeserializerValueUTF8<TEntity> : IDeserializer<TEntity>
+{
+    private readonly Encoding _encoder;
+    private readonly JsonCreationConverter<TEntity> _customCreationConverter;
+    public JsonDeserializerValueUTF8(JsonCreationConverter<TEntity> customCreationConverter)
+    {
+        _encoder = Encoding.UTF8;
+        _customCreationConverter = customCreationConverter;
+    }
+    public TEntity Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
+    {
+        return JsonConvert.DeserializeObject<TEntity>(_encoder.GetString(data.ToArray()));
+    }
+}
